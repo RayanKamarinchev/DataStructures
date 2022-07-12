47c452e363f8266c4a7d1e30a0d8732335e40d19
RayanKamarinchev <rayan2008bg@gmail.com>
2022-07-13 00:20:54 +0300

    FINALLY A BINARY TREEE

---
+++ DataStructures/BinaryTree.cs
@@ -0,0 +1,154 @@
+﻿namespace DataStructures
+{
+    public class BinaryTree
+    {
+        public class Node
+        {
+            public int Data { get; set; }
+            public Node? Left { get; set; } = null;
+            public Node? Right { get; set; } = null;
+
+            public Node(int data)
+            {
+                Data = data;
+            }
+        }
+
+        private Node? root = null;
+
+        public void Insert(int num)
+        {
+            if (root == null)
+            {
+                root = new Node(num);
+                return;
+            }
+
+            Node? target = root;
+            Node? oldTarget = target;
+            while (target!=null)
+            {
+                oldTarget = target;
+                int result = Compare(target, num);
+                if (result==1)
+                {
+                    target = target.Right;
+                }
+                else if (result == -1)
+                {
+                    target = target.Left;
+                }
+            }
+
+            int lastResult = Compare(oldTarget, num);
+            if (lastResult == 1)
+            {
+                oldTarget.Right = new Node(num);
+            }
+            else if (lastResult == -1)
+            {
+                oldTarget.Left = new Node(num);
+            }
+        }
+
+        public Node[] Find(int num)
+        {
+            if (root == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            Node? target = root;
+            Node? oldTarget = target;
+            while (target != null)
+            {
+                int result = Compare(target, num);
+                if (result == 1)
+                {
+                    oldTarget = target;
+                    target = target.Right;
+                }
+                else if (result == -1)
+                {
+                    oldTarget = target;
+                    target = target.Left;
+                }
+                else
+                {
+                    return new Node[2]{oldTarget, target};
+                }
+            }
+            throw new InvalidOperationException();
+        }
+
+        public void Remove(int num)
+        {
+            Node[] nodes = Find(num);
+            Node parentTarget = nodes[0];
+            Node target = nodes[1];
+            bool isLeft = parentTarget.Left == target;
+            if (target.Right == null && target.Left == null)
+            {
+                if (isLeft)
+                {
+                    parentTarget.Left = null;
+                }
+                else
+                {
+                    parentTarget.Right = null;
+                }
+                return;
+            }
+
+            if (target.Right == null)
+            {
+                if (isLeft)
+                {
+                    parentTarget.Left = target.Left;
+                }
+                else
+                {
+                    parentTarget.Right = target.Left;
+                }
+                return;
+            }
+
+            if (target.Left == null)
+            {
+                if (isLeft)
+                {
+                    parentTarget.Left = target.Right;
+                }
+                else
+                {
+                    parentTarget.Right = target.Right;
+                }
+                return;
+            }
+
+            Node? clonetarget = target.Right;
+            Node? clonetargetParent = target;
+            while (clonetarget.Left!=null)
+            {
+                clonetargetParent = clonetarget;
+                clonetarget = clonetarget.Left;
+            }
+
+            target.Data = clonetarget.Data;
+            clonetargetParent.Right = clonetarget.Right;
+        }
+
+        private int Compare(Node target, int value)
+        {
+            if (target.Data > value)
+            {
+                return -1;
+            }
+            if (target.Data < value)
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,50 +1,19 @@
 ﻿using DataStructures;
 
-MyHeap heap = new MyHeap();
-heap.Add(1);
-heap.Add(5);
-heap.Add(1);
-heap.Add(8);
-heap.Add(6);
-heap.Add(2);
-heap.Add(2);
-heap.Add(13);
-heap.Add(12);
-heap.Add(11);
-heap.Add(7);
-heap.Add(2);
-heap.Add(15);
-heap.Add(3);
-heap.Add(10);
+BinaryTree binaryTree = new BinaryTree();
+binaryTree.Insert(80);
+binaryTree.Insert(4);
+binaryTree.Insert(2);
+binaryTree.Insert(-1);
+binaryTree.Insert(0);
+binaryTree.Insert(14);
+binaryTree.Insert(6);
+binaryTree.Insert(9);
+binaryTree.Insert(8);
+binaryTree.Insert(13);
+binaryTree.Insert(12);
+binaryTree.Insert(15);
+binaryTree.Insert(16);
 
-heap.Poll();
-Print();
-heap.Remove(12);
-Print();
-heap.Remove(3);
-Print();
-heap.Poll();
-Print();
-heap.Remove(6);
-Print();
-
-bool IsPowerOfTwo(int x)
-{
-    return (x & (x - 1)) == 0;
-}
-
-void Print()
-{
-    for (int i = 0; i < heap.Count; i++)
-    {
-        Console.Write(heap[i] + " ");
-        if (IsPowerOfTwo(i + 2))
-        {
-            Console.WriteLine();
-        }
-    }
-
-    Console.WriteLine();
-    Console.WriteLine();
-    Console.WriteLine();
-}
+binaryTree.Remove(14);
+Console.WriteLine("sus");
